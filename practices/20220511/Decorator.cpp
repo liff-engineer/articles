2dbd2216c0550247b0456f1a7e079332b8484d55
Lee <liff.engineer@gmail.com>
2022-05-16 17:26:40 +0800

    proxy template class

--- practices/20220511/Decorator.cpp
+++ practices/20220511/Decorator.cpp
@@ -31,237 +31,410 @@ namespace impl {
     };
 }
 
-/// @brief 外部API
-class EntityProxy {
-public:
-    EntityProxy() = default;
-    explicit EntityProxy(IValue* value)
-        :m_v(value)
-    {
-        assert(m_v != nullptr);
-    }
+namespace v1 {
 
-    //虽然有虚函数,但是应用场景是有保证的,因而把5件套全实现了
-    EntityProxy(const EntityProxy& other) = default;
-    EntityProxy& operator=(const EntityProxy& other) = default;
-    EntityProxy(EntityProxy&& other) noexcept = default;
-    EntityProxy& operator=(EntityProxy && other) noexcept = default;
-    virtual ~EntityProxy() = default;
+    /// @brief 外部API
+    class EntityProxy {
+    public:
+        EntityProxy() = default;
+        explicit EntityProxy(IValue* value)
+            :m_v(value)
+        {
+            assert(m_v != nullptr);
+        }
 
-    explicit  operator bool() const noexcept {
-        return IsValidImpl();
-    }
+        //虽然有虚函数,但是应用场景是有保证的,因而把5件套全实现了
+        EntityProxy(const EntityProxy& other) = default;
+        EntityProxy& operator=(const EntityProxy& other) = default;
+        EntityProxy(EntityProxy&& other) noexcept = default;
+        EntityProxy& operator=(EntityProxy&& other) noexcept = default;
+        virtual ~EntityProxy() = default;
 
-    template<typename T>
-    T  As() const {
-        return T{ m_v };
-    }
-protected:
-    IValue* m_v{};
-    virtual bool IsValidImpl() const noexcept {
-        return m_v != nullptr;
-    }
-};
+        explicit  operator bool() const noexcept {
+            return IsValidImpl();
+        }
 
-
-class MyObjectProxy : public EntityProxy {
-public:
-    static impl::MyObjectImpl* To(IValue* v) {
-        return dynamic_cast<impl::MyObjectImpl*>(v);
-    }
-    static bool Is(IValue* v) {
-        return To(v) != nullptr;
-    }
-public:
-    using EntityProxy::EntityProxy;
-    MyObjectProxy() = default;
-    explicit MyObjectProxy(IValue* v)
-        :EntityProxy(v)
-    {
-        //这个函数有副作用,实际实现时采用抛出异常的策略
-        m_impl = To(v);
-        assert(m_impl != nullptr);
-    }
-
-    int iV() const {
-        return m_impl->iV();
-    }
-    double dV() const {
-        return m_impl->dV();
-    }
-    void iV(int v) {
-        m_impl->iV(v);
-    }
-    void dV(double v) {
-        m_impl->dV(v);
-    }
-protected:
-    impl::MyObjectImpl* m_impl{};
-    bool IsValidImpl() const noexcept override {
-        return EntityProxy::IsValidImpl() && (m_impl != nullptr);
-    }
-};
-
-
-/////////////////////////////////////////////////
-///  以下是基于派生方式扩展API以及对象类型
-
-class MyObjectAddApiProxy :public  MyObjectProxy {
-public:
-    using MyObjectProxy::MyObjectProxy;
-
-    std::pair<int,double> Values() const{
-        return std::make_pair(iV(),dV());
-    }
-
-    void setValues(int iv, double dv) {
-        iV(iv);
-        dV(dv);
-    }
-};
-
-class MyObjectAddCheckProxy :public MyObjectProxy {
-public:
-    static bool Is(const MyObjectProxy& obj) {
-        return obj.iV() > 256;
-    }
-    static bool Is(IValue* v) {
-        if (!MyObjectProxy::Is(v)) return false;
-        return Is(MyObjectProxy{ v });
-    }
-public:
-    MyObjectAddCheckProxy() = default;
-    explicit MyObjectAddCheckProxy(IValue* v)
-        :MyObjectProxy(v) {
-        assert(Is(*this));
+        template<typename T>
+        T  As() const {
+            return T{ m_v };
+        }
+    protected:
+        IValue* m_v{};
+        virtual bool IsValidImpl() const noexcept {
+            return m_v != nullptr;
+        }
     };
-protected:
-    bool IsValidImpl() const noexcept override {
-        return MyObjectProxy::IsValidImpl() && (Is(*this));
+
+
+    class MyObjectProxy : public EntityProxy {
+    public:
+        static impl::MyObjectImpl* To(IValue* v) {
+            return dynamic_cast<impl::MyObjectImpl*>(v);
+        }
+        static bool Is(IValue* v) {
+            return To(v) != nullptr;
+        }
+    public:
+        using EntityProxy::EntityProxy;
+        MyObjectProxy() = default;
+        explicit MyObjectProxy(IValue* v)
+            :EntityProxy(v)
+        {
+            //这个函数有副作用,实际实现时采用抛出异常的策略
+            m_impl = To(v);
+            assert(m_impl != nullptr);
+        }
+
+        int iV() const {
+            return m_impl->iV();
+        }
+        double dV() const {
+            return m_impl->dV();
+        }
+        void iV(int v) {
+            m_impl->iV(v);
+        }
+        void dV(double v) {
+            m_impl->dV(v);
+        }
+    protected:
+        impl::MyObjectImpl* m_impl{};
+        bool IsValidImpl() const noexcept override {
+            return EntityProxy::IsValidImpl() && (m_impl != nullptr);
+        }
+    };
+
+
+    /////////////////////////////////////////////////
+    ///  以下是基于派生方式扩展API以及对象类型
+
+    class MyObjectAddApiProxy :public  MyObjectProxy {
+    public:
+        using MyObjectProxy::MyObjectProxy;
+
+        std::pair<int, double> Values() const {
+            return std::make_pair(iV(), dV());
+        }
+
+        void setValues(int iv, double dv) {
+            iV(iv);
+            dV(dv);
+        }
+    };
+
+    class MyObjectAddCheckProxy :public MyObjectProxy {
+    public:
+        static bool Is(const MyObjectProxy& obj) {
+            return obj.iV() > 256;
+        }
+        static bool Is(IValue* v) {
+            if (!MyObjectProxy::Is(v)) return false;
+            return Is(MyObjectProxy{ v });
+        }
+    public:
+        MyObjectAddCheckProxy() = default;
+        explicit MyObjectAddCheckProxy(IValue* v)
+            :MyObjectProxy(v) {
+            assert(Is(*this));
+        };
+    protected:
+        bool IsValidImpl() const noexcept override {
+            return MyObjectProxy::IsValidImpl() && (Is(*this));
+        }
+    };
+
+    /////////////////////////////////////////////////
+    ///  以下是基于组合方式扩展API以及对象类型
+
+    class MyObjectAddApiDecorator {
+        MyObjectProxy m_obj;
+    public:
+        static bool Is(IValue* v) {
+            return MyObjectProxy::Is(v);
+        }
+    public:
+        MyObjectAddApiDecorator() = default;
+
+        explicit MyObjectAddApiDecorator(IValue* v)
+            :m_obj(v) {};
+
+        MyObjectProxy* operator ->() noexcept {
+            return std::addressof(m_obj);
+        }
+
+        const MyObjectProxy* operator ->() const noexcept {
+            return std::addressof(m_obj);
+        }
+
+        explicit operator bool() const noexcept {
+            return m_obj.operator bool();
+        }
+
+        std::pair<int, double> Values() const {
+            return std::make_pair(m_obj.iV(), m_obj.dV());
+        }
+
+        void setValues(int iv, double dv) {
+            m_obj.iV(iv);
+            m_obj.dV(dv);
+        }
+    };
+
+    class MyObjectAddCheckDecorator {
+        MyObjectProxy m_obj;
+    public:
+        static bool Is(const MyObjectProxy& obj) {
+            return obj.iV() > 256;
+        }
+        static bool Is(IValue* v) {
+            if (!MyObjectProxy::Is(v)) return false;
+            return Is(MyObjectProxy{ v });
+        }
+    public:
+        MyObjectAddCheckDecorator() = default;
+        explicit MyObjectAddCheckDecorator(IValue* v)
+            :m_obj(v) {
+            assert(Is(m_obj));
+        }
+
+        MyObjectProxy* operator ->() noexcept {
+            return std::addressof(m_obj);
+        }
+
+        const MyObjectProxy* operator ->() const noexcept {
+            return std::addressof(m_obj);
+        }
+
+        explicit operator bool() const noexcept {
+            return m_obj && Is(m_obj);
+        }
+    };
+
+    //Proxy和Decorator还是有相对明显的差别的
+    //这个差别可以以写法论(继承、组合)
+    //也可以以目的论(转发动作、扩展能力)
+    //
+    //以目的论较为贴合使用场景,这里以目的论,除了EntityProxy
+    //其它都应该算Decorator
+
+    void test(IValue* v) {
+        if (!Is<MyObjectProxy>(v)) {
+            std::cout << "!Is<MyObject>\n";
+            return;
+        }
+        auto obj = As<MyObjectProxy>(v);
+        std::cout << obj.iV() << "\n";
+        std::cout << obj.dV() << "\n";
+        obj.iV(128);
+        obj.dV(obj.dV() * 2);
+        std::cout << obj.iV() << "\n";
+        std::cout << obj.dV() << "\n";
+
+        //测试代理方式-附加API
+        {
+            auto r = obj.As<MyObjectAddApiProxy>();
+            auto values = r.Values();
+            std::cout << "(" << values.first << "," << values.second << ")" << "\n";
+        }
+        //测试修饰器方式-附加API
+        {
+            auto r = obj.As<MyObjectAddApiDecorator>();
+            std::cout << "(" << r->iV() << "," << r->dV() << ")" << "\n";
+        }
+
+        if (!Is<MyObjectAddApiProxy>(v)) {
+            std::cout << "!Is<MyObjectAddApiProxy>\n";
+        }
+        else
+        {
+            std::cout << "Is<MyObjectAddApiProxy>\n";
+        }
+
+        if (!Is<MyObjectAddCheckProxy>(v)) {
+            std::cout << "!Is<MyObjectAddCheckProxy>\n";
+        }
+        else
+        {
+            std::cout << "Is<MyObjectAddCheckProxy>\n";
+        }
+
+        if (!Is<MyObjectAddCheckDecorator>(v)) {
+            std::cout << "!Is<MyObjectAddCheckDecorator>\n";
+        }
+        else
+        {
+            std::cout << "Is<MyObjectAddCheckDecorator>\n";
+        }
     }
-};
 
-/////////////////////////////////////////////////
-///  以下是基于组合方式扩展API以及对象类型
-
-class MyObjectAddApiDecorator {
-    MyObjectProxy m_obj;
-public:
-    static bool Is(IValue* v) {
-        return MyObjectProxy::Is(v);
-    }
-public:
-    MyObjectAddApiDecorator() = default;
-
-    explicit MyObjectAddApiDecorator(IValue* v)
-        :m_obj(v) {};
-
-    MyObjectProxy* operator ->() noexcept {
-        return std::addressof(m_obj);
-    }
-
-    const MyObjectProxy* operator ->() const noexcept {
-        return std::addressof(m_obj);
-    }
-
-    explicit operator bool() const noexcept {
-        return m_obj.operator bool();
-    }
-
-    std::pair<int, double> Values() const {
-        return std::make_pair(m_obj.iV(), m_obj.dV());
-    }
-
-    void setValues(int iv, double dv) {
-        m_obj.iV(iv);
-        m_obj.dV(dv);
-    }
-};
-
-class MyObjectAddCheckDecorator {
-    MyObjectProxy m_obj;
-public:
-    static bool Is(const MyObjectProxy& obj) {
-        return obj.iV() > 256;
-    }
-    static bool Is(IValue* v) {
-        if (!MyObjectProxy::Is(v)) return false;
-        return Is(MyObjectProxy{ v });
-    }
-public:
-    MyObjectAddCheckDecorator() = default;
-    explicit MyObjectAddCheckDecorator(IValue* v)
-        :m_obj(v) {
-        assert(Is(m_obj));
-    }
-
-    MyObjectProxy* operator ->() noexcept {
-        return std::addressof(m_obj);
-    }
-
-    const MyObjectProxy* operator ->() const noexcept {
-        return std::addressof(m_obj);
-    }
-
-    explicit operator bool() const noexcept {
-        return m_obj && Is(m_obj);
-    }
-};
-
-//Proxy和Decorator还是有相对明显的差别的
-//这个差别可以以写法论(继承、组合)
-//也可以以目的论(转发动作、扩展能力)
-//
-//以目的论较为贴合使用场景,这里以目的论,除了EntityProxy
-//其它都应该算Decorator
-
-void test(IValue* v) {
-    if (!Is<MyObjectProxy>(v)) {
-        std::cout << "!Is<MyObject>\n";
-        return;
-    }
-    auto obj = As<MyObjectProxy>(v);
-    std::cout << obj.iV() << "\n";
-    std::cout << obj.dV() << "\n";
-    obj.iV(128);
-    obj.dV(obj.dV() * 2);
-    std::cout << obj.iV() << "\n";
-    std::cout << obj.dV() << "\n";
-
-    //测试代理方式-附加API
+    void RunTest(IValue& obj)
     {
-        auto r = obj.As<MyObjectAddApiProxy>();
-        auto values = r.Values();
-        std::cout <<"("<<values.first<<","<<values.second<<")" << "\n";
+
+        EntityProxy e1{};
+        MyObjectAddApiProxy e2{};
+        MyObjectAddApiProxy e2c = e2;
+        MyObjectAddCheckProxy e3{};
+
+        test(&obj);
     }
-    //测试修饰器方式-附加API
-    {
-        auto r = obj.As<MyObjectAddApiDecorator>();
-        std::cout << "(" << r->iV() << "," << r->dV() << ")" << "\n";
+}
+
+#include "Proxy.hpp"
+namespace v2
+{
+    class ObjectProxy :public abc::Proxy<IValue*> {
+    public:
+        static impl::MyObjectImpl* To(IValue* v) {
+            return dynamic_cast<impl::MyObjectImpl*>(v);
+        }
+        static bool Is(IValue* v) {
+            return To(v) != nullptr;
+        }
+    public:
+        using Super::Super;
+        ObjectProxy() = default;
+        explicit ObjectProxy(IValue* v)
+            :Super(v), m_impl(To(v))
+        {}
+
+        int iV() const {
+            return m_impl->iV();
+        }
+        double dV() const {
+            return m_impl->dV();
+        }
+
+        void iV(int v) {
+            m_impl->iV(v);
+        }
+        void dV(double v) {
+            m_impl->dV(v);
+        }
+    protected:
+        impl::MyObjectImpl* m_impl{};
+        bool IsValidImpl() const noexcept override {
+            return Super::IsValidImpl() && (m_impl != nullptr);
+        }
+    };
+
+    /////////////////////////////////////////////////
+    ///  以下是基于派生方式扩展API以及对象类型
+
+    class ObjectAddApiProxy :public  ObjectProxy {
+    public:
+        using ObjectProxy::ObjectProxy;
+
+        std::pair<int, double> Values() const {
+            return std::make_pair(iV(), dV());
+        }
+
+        void setValues(int iv, double dv) {
+            iV(iv);
+            dV(dv);
+        }
+    };
+
+    class MyObjectAddCheckProxy :public ObjectProxy {
+    public:
+        static bool Is(const ObjectProxy& obj) {
+            return obj.iV() > 256;
+        }
+        static bool Is(IValue* v) {
+            if (!ObjectProxy::Is(v)) return false;
+            return Is(ObjectProxy{ v });
+        }
+    public:
+        MyObjectAddCheckProxy() = default;
+        explicit MyObjectAddCheckProxy(IValue* v)
+            :ObjectProxy(v) {
+            assert(Is(*this));
+        };
+    protected:
+        bool IsValidImpl() const noexcept override {
+            return ObjectProxy::IsValidImpl() && (Is(*this));
+        }
+    };
+
+    /////////////////////////////////////////////////
+    ///  以下是基于组合方式扩展API以及对象类型
+
+    class MyObjectAddApiDecorator :public abc::Proxy<ObjectProxy> {
+    public:
+        static bool Is(IValue* v) {
+            return ObjectProxy::Is(v);
+        }
+    public:
+        MyObjectAddApiDecorator() = default;
+        explicit MyObjectAddApiDecorator(IValue* v)
+            :Super(ObjectProxy(v)) {};
+
+        std::pair<int, double> Values() const {
+            return std::make_pair(Get().iV(), Get().dV());
+        }
+
+        void setValues(int iv, double dv) {
+            Get().iV(iv);
+            Get().dV(dv);
+        }
+    };
+
+    void test(IValue* v) {
+        if (!Is<ObjectProxy>(v)) {
+            std::cout << "!Is<MyObject>\n";
+            return;
+        }
+        auto obj = As<ObjectProxy>(v);
+        std::cout << obj.iV() << "\n";
+        std::cout << obj.dV() << "\n";
+        obj.iV(128);
+        obj.dV(obj.dV() * 2);
+        std::cout << obj.iV() << "\n";
+        std::cout << obj.dV() << "\n";
+
+        //测试代理方式-附加API
+        {
+            auto r = ObjectAddApiProxy(v);
+            auto values = r.Values();
+            std::cout << "(" << values.first << "," << values.second << ")" << "\n";
+        }
+        //测试修饰器方式-附加API
+        {
+            auto r = MyObjectAddApiDecorator(v);
+            std::cout << "(" << r->iV() << "," << r->dV() << ")" << "\n";
+        }
+
+        if (!Is<ObjectAddApiProxy>(v)) {
+            std::cout << "!Is<MyObjectAddApiProxy>\n";
+        }
+        else
+        {
+            std::cout << "Is<MyObjectAddApiProxy>\n";
+        }
+
+        if (!Is<MyObjectAddCheckProxy>(v)) {
+            std::cout << "!Is<MyObjectAddCheckProxy>\n";
+        }
+        else
+        {
+            std::cout << "Is<MyObjectAddCheckProxy>\n";
+        }
+
+        if (!Is<MyObjectAddApiDecorator>(v)) {
+            std::cout << "!Is<MyObjectAddCheckDecorator>\n";
+        }
+        else
+        {
+            std::cout << "Is<MyObjectAddCheckDecorator>\n";
+        }
     }
 
-    if (!Is<MyObjectAddApiProxy>(v)) {
-        std::cout << "!Is<MyObjectAddApiProxy>\n";
-    }
-    else
+    void RunTest(IValue& obj)
     {
-        std::cout << "Is<MyObjectAddApiProxy>\n";
-    }
+        ObjectAddApiProxy e2{};
+        ObjectAddApiProxy e2c = e2;
+        MyObjectAddCheckProxy e3{};
 
-    if (!Is<MyObjectAddCheckProxy>(v)) {
-        std::cout << "!Is<MyObjectAddCheckProxy>\n";
-    }
-    else
-    {
-        std::cout << "Is<MyObjectAddCheckProxy>\n";
-    }
-
-    if (!Is<MyObjectAddCheckDecorator>(v)) {
-        std::cout << "!Is<MyObjectAddCheckDecorator>\n";
-    }
-    else
-    {
-        std::cout << "Is<MyObjectAddCheckDecorator>\n";
+        test(&obj);
     }
 }
 
@@ -269,12 +442,7 @@ int main() {
     impl::MyObjectImpl obj{};
     obj.iV(1024);
     obj.dV(3.1415);
-
-    EntityProxy e1{};
-    MyObjectAddApiProxy e2{};
-    MyObjectAddApiProxy e2c = e2;
-    MyObjectAddCheckProxy e3{};
-
-    test(&obj);
+    v1::RunTest(obj);
+    v2::RunTest(obj);
     return 0;
 }
---
+++ practices/20220511/Proxy.hpp
@@ -0,0 +1,64 @@
+﻿#pragma once
+#include <type_traits>
+
+namespace abc
+{
+    /// @brief 类型T的代理类
+    /// @tparam T 指针类型或者可以转换为bool的类型
+    template<typename T>
+    class Proxy {
+        static_assert(std::is_pointer<T>::value
+            || (std::is_constructible<bool, T>::value
+                && !std::is_convertible<T, bool>::value
+                ), "T should be pointer type or has `explicit operator bool()`!");
+    public:
+        Proxy() = default;
+        explicit Proxy(T obj) :m_obj(std::move(obj)) {};
+
+        Proxy(const Proxy& other) = default;
+        Proxy& operator=(const Proxy& other) = default;
+        Proxy(Proxy&& other) noexcept = default;
+        Proxy& operator=(Proxy&& other) noexcept = default;
+
+        virtual ~Proxy() = default;
+
+        auto operator->() noexcept { return GetPointer(this); }
+        auto operator->() const noexcept { return GetPointer(this); }
+
+        decltype(auto) Get() noexcept { return GetImpl(this); }
+        decltype(auto) Get() const noexcept { return GetImpl(this); }
+
+        explicit operator bool() const noexcept {
+            if (!m_obj) return false;
+            return IsValidImpl();
+        }
+    protected:
+        using Super = Proxy<T>;
+        virtual bool IsValidImpl() const noexcept {
+            return true;
+        }
+        T m_obj{};
+    private:
+        //保证获取正确的类型;
+        //技术细节参见C++标准提案P0847(Deducing this)的"Motivation"章节
+        template<typename U>
+        static decltype(auto) GetImpl(U&& vp) {
+            return vp->m_obj;
+        }
+
+        template<typename U>
+        static decltype(auto) GetPointer(U&& vp) {
+            return GetPointerImpl(std::forward<U>(vp), std::is_pointer<T>{});
+        }
+
+        template<typename U>
+        static decltype(auto) GetPointerImpl(U&& vp, std::true_type) {
+            return vp->m_obj;
+        }
+
+        template<typename U>
+        static decltype(auto) GetPointerImpl(U&& vp, std::false_type) {
+            return std::addressof(vp->m_obj);
+        }
+    };
+}
