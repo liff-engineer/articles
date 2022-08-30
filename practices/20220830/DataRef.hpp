533ebce815a642bba0ab8b8c2a861092fa487e89
Lee <liff.engineer@gmail.com>
2022-08-30 23:14:56 +0800

    data ref design

---
+++ practices/20220830/DataRef.hpp
@@ -0,0 +1,65 @@
+﻿/// 数据引用: 包装类中通过Set/Get接口暴露出的数据，以支持提取算法等公共操作
+
+#pragma once
+#include <functional>
+#include <memory>
+
+namespace abc
+{
+    template<typename T>
+    class DataRef {
+        std::function<T()> m_reader;
+        std::function<bool(const T&)> m_writer;
+    public:
+        template<typename R, typename W>
+        explicit DataRef(R&& r, W&& w)
+            :m_reader(std::forward<R>(r)), m_writer(std::forward<W>(w)) {};
+
+        explicit operator bool() const noexcept {
+            return m_reader && m_writer;
+        }
+
+        T Get() {
+            return m_reader();
+        }
+
+        bool Set(const T& obj) {
+            return m_writer(obj);
+        }
+    };
+
+    template<typename T>
+    DataRef<T> MakeDataRef(T& obj) {
+        return DataRef<T>{
+            [&]()->T {
+                return obj;
+            },
+                [&](const T& v)->bool {
+                obj = v;
+                return true;
+            }
+        };
+    }
+
+    template<typename T, typename U, typename R, typename W>
+    DataRef<T> MakeDataRef(U& obj, R&& r, W&& w) {
+        return DataRef<T>{
+            [&, rf = std::forward<R>(r)]()->T {
+                return rf(obj);
+            },
+                [&, wf = std::forward<W>(w)](const T& v)->bool {
+                wf(obj, v);
+                return true;
+            }
+        };
+    }
+
+    template<typename T>
+    class IDataRef {
+    public:
+        virtual ~IDataRef() = default;
+
+        virtual T Get() const = 0;
+        virtual bool Set(const T& v) = 0;
+    };
+}
---
+++ practices/20220830/DataRefApp.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <string>
+#include "DataRef.hpp"
+
+using namespace abc;
+
+class MyObject {
+    std::string m_str;
+public:
+    std::string GetStr() const {
+        return m_str;
+    }
+
+    void SetStr(const std::string& v) {
+        m_str = v;
+    }
+};
+
+class MyObjectStrMemberRef final :public IDataRef<std::string>
+{
+    MyObject* m_obj;
+public:
+    explicit MyObjectStrMemberRef(MyObject& obj)
+        :m_obj(&obj) {};
+
+    std::string Get() const override {
+        return m_obj->GetStr();
+    }
+
+    bool Set(const std::string& v) override {
+        m_obj->SetStr(v);
+        return true;
+    }
+};
+
+constexpr auto n = sizeof(MyObjectStrMemberRef);
+constexpr auto n1 = sizeof(MyObject*);
+
+int main() {
+    int iV = 10;
+
+    DataRef<int> iObj = MakeDataRef(iV);
+    std::cout << iObj.Get() << "\n";
+    iObj.Set(1024);
+
+    MyObject obj{};
+    DataRef<std::string> strObj = MakeDataRef<std::string>(obj,
+        std::mem_fn(&MyObject::GetStr),
+        std::mem_fn(&MyObject::SetStr)
+        );
+    strObj.Set("liff.engineer@gmail.com");
+
+    std::unique_ptr<IDataRef<std::string>> mRef = std::make_unique<MyObjectStrMemberRef>(obj);
+    mRef->Set("liff.engineer");
+    return 0;
+}
